e4a5c161000e5e2feb98d9c073a105b1aede6236
wuningjian <956980241@qq.com>
2017-02-14 16:32:41 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by oldfive on 2017/2/9.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("This is main MavenJava");
+
+    }
+}
---
+++ src/main/java/com/wuningjian/java/HttpClientTest.java
@@ -0,0 +1,12 @@
+package com.wuningjian.java;
+
+import org.apache.http.client.HttpClient;
+
+/**
+ * Created by oldfive on 2017/2/9.
+ */
+public class HttpClientTest {
+    public void HttpClientPost(){
+
+    }
+}
